7dbf0cfb0835f73f3e7f926a9bf3d7c0eab62700
Paper_Xiang <89193024+PaperXiang@users.noreply.github.com>
2024-08-21 11:13:34 +0800

    fix

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -13,7 +13,7 @@ export const NAV_DATA: NavData[] = [
         icon: 'https://www.emojiall.com/images/240/apple/1f97a.png',
         title: 'gongmin正传',
         desc: '心态硬汉',
-        link: '/imcwiki/article/gongmin传.md',
+        link: '/imcwiki/article/gongmin传',
       },
     ],
   },
