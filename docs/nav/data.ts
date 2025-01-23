a2f81bbfc30a03d766d5359982dfd655626030d5
Paper_Xiang <89193024+PaperXiang@users.noreply.github.com>
2025-01-23 17:48:49 +0800

    nav

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -7,6 +7,41 @@ type NavData = {
 
 export const NAV_DATA: NavData[] = [
   {
+    title: '猪头肉的青蛙池子',
+    items: [
+      {
+        icon: 'https://www.emojiall.com/images/60/apple/1f437.png',
+        title: 'Arvuelut传',
+        desc: '猪头肉无需多言',
+        link: '/imcwiki/article/ar传',
+      },
+      {
+        icon: 'https://www.emojiall.com/images/60/apple/1f437.png',
+        title: '4799传',
+        desc: '残疾人骑士大神',
+        link: '/imcwiki/article/4799传',
+      },
+      {
+        icon: 'https://www.emojiall.com/images/60/apple/1f92d.png',
+        title: 'Spirit_Snow(天山苍雪)传',
+        desc: '护花使者,盒子涝坡',
+        link: '/imcwiki/article/天山苍雪传',
+      },
+      {
+        icon: 'https://www.emojiall.com/images/240/apple/1f97a.png',
+        title: '111传',
+        desc: '复读机',
+        link: '/imcwiki/article/111传',
+      },
+      {
+        icon: 'https://www.emojiall.com/images/240/apple/1f97a.png',
+        title: '颜曦、传',
+        desc: '复读机',
+        link: '/imcwiki/article/颜曦传',
+      },
+    ],
+  },
+  {
     title: '唐氏兄弟',
     items: [
       {
@@ -18,7 +53,7 @@ export const NAV_DATA: NavData[] = [
       {
         icon: 'https://www.emojiall.com/images/240/apple/1f97a.png',
         title: 'YOYOO JS传',
-        desc: '我支持gongmin',
+        desc: '还是tai°乐子',
         link: '/imcwiki/article/gongmin传',
       },
     ],
@@ -43,12 +78,6 @@ export const NAV_DATA: NavData[] = [
         title: 'Foursickles_传',
         desc: '香肠使用者',
         link: '/imcwiki/article/foursickles传',
-      },
-      {
-        icon: 'https://www.emojiall.com/images/240/apple/1f97a.png',
-        title: '111传',
-        desc: '复读机',
-        link: '/imcwiki/article/111传',
       },
     ],
   },
@@ -146,14 +175,8 @@ export const NAV_DATA: NavData[] = [
       },
       {
         icon: 'https://www.emojiall.com/images/60/apple/1f92d.png',
-        title: 'Spirit_Snow(天山苍雪)传',
-        desc: '猪头肉老公',
-        link: '/imcwiki/article/天山苍雪传',
-      },
-      {
-        icon: 'https://www.emojiall.com/images/60/apple/1f92d.png',
         title: 'OF_DeevYeeish822传',
-        desc: '猪头肉老公',
+        desc: 'lj',
         link: '/imcwiki/article/OF_DeevYeeish822传',
       },
     ],
@@ -161,12 +184,6 @@ export const NAV_DATA: NavData[] = [
   {
     title: '逆天乐子',
     items: [
-      {
-        icon: 'https://www.emojiall.com/images/60/apple/1f437.png',
-        title: 'Arvuelut传',
-        desc: '猪头肉无需多言',
-        link: '/imcwiki/article/ar传',
-      },
       {
         icon: 'https://www.emojiall.com/images/60/apple/1f60e.png',
         title: 'guisama传',
