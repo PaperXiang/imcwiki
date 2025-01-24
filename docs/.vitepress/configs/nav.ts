612ffcaad24e73b84c3253c11c09b32150cfdda5
Paper_Xiang <89193024+PaperXiang@users.noreply.github.com>
2025-01-24 16:55:31 +0800

    fix

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -2,5 +2,6 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
   { text: 'IMC乐子导航', link: '/nav/' },
+  
   { text: '百科主页', link: 'https://paperxiang.github.io/imcwiki/' },
 ]
