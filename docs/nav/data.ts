7fe6cf8d8c5937a59ead98647dd30751d9a267df
Paper_Xiang <89193024+PaperXiang@users.noreply.github.com>
2025-01-24 09:31:43 +0000

    fix

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -7,41 +7,6 @@ type NavData = {
 
 export const NAV_DATA: NavData[] = [
   {
-    title: '猪头肉的青蛙池子',
-    items: [
-      {
-        icon: 'https://www.emojiall.com/images/60/apple/1f437.png',
-        title: 'Arvuelut传',
-        desc: '猪头肉无需多言',
-        link: '/imcwiki/article/ar传',
-      },
-      {
-        icon: 'https://www.emojiall.com/images/60/apple/1f437.png',
-        title: '4799传',
-        desc: '残疾人骑士大神',
-        link: '/imcwiki/article/4799传',
-      },
-      {
-        icon: 'https://www.emojiall.com/images/60/apple/1f92d.png',
-        title: 'Spirit_Snow(天山苍雪)传',
-        desc: '护花使者,盒子涝坡',
-        link: '/imcwiki/article/天山苍雪传',
-      },
-      {
-        icon: 'https://www.emojiall.com/images/240/apple/1f97a.png',
-        title: '111传',
-        desc: '复读机',
-        link: '/imcwiki/article/111传',
-      },
-      {
-        icon: 'https://www.emojiall.com/images/240/apple/1f97a.png',
-        title: '颜曦、传',
-        desc: '复读机',
-        link: '/imcwiki/article/颜曦传',
-      },
-    ],
-  },
-  {
     title: '唐氏兄弟',
     items: [
       {
